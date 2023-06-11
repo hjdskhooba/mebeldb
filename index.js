13b1e0b1c33b2bf94a9ae4f5ac5c2d383784c0e3
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-06-11 08:32:05 +0600

    add json-server-auth

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const jsonServer = require("json-server");
 const jsonServerAuth = require("json-server-auth");
 
 const server = jsonServer.create();
-const authRouter = jsonServerAuth.router("db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 2222;
 
-server.use(middlewares);
-server.db = authOptions.authFile;
+server.db = router.db;
+
 const authOptions = {
-  authFile: "users.json",
-  authPropertyName: "username",
+  auth: jsonServerAuth,
+  authConfig: {
+    users: "users.json"
+  },
+  noCors: false,
+  static: "./public"
 };
-authRouter.use(jsonServerAuth(authOptions));
-server.use(authRouter);
+
+server.use(middlewares);
+server.use(authOptions.auth(authOptions.authConfig));
+server.use(router);
 
 server.listen(port, () => {
-  console.log("JSON Server is running on port " + port);
+  console.log("JSON Server with json-server-auth is running on port " + port);
 });
