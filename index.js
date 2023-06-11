2cc2b9660d14663eb41ef8fd27aabcdc22fef48c
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-06-11 08:28:09 +0600

    ph

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const jsonServer = require("json-server");
+const jsonServerAuth = require("json-server-auth");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const authRouter = jsonServerAuth.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.port || 2222;
+
+const port = process.env.PORT || 2222;
+
 server.use(middlewares);
-server.use(router);
+server.db = authOptions.authFile;
+const authOptions = {
+  authFile: "users.json",
+  authPropertyName: "username",
+};
+authRouter.use(jsonServerAuth(authOptions));
+server.use(authRouter);
+
 server.listen(port, () => {
-  console.log("JSON Server is running " + port);
+  console.log("JSON Server is running on port " + port);
 });
